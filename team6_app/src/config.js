65d470c90db2ac44ffeafe3eb451227202232ca0
Sarah Oh <syoh@bu.edu>
2020-03-25 00:22:49 -0400

    Spotify API with error

---
+++ team6_app/src/config.js
@@ -0,0 +1,10 @@
+export const authEndpoint = "https://accounts.spotify.com/authorize";
+
+// Replace with your app's client ID, redirect URI and desired scopes
+export const clientId = "501034e7633c410bb4b3955046a6c20f";
+export const redirectUri = "http://localhost:3000";
+export const scopes = [
+    "user-top-read",
+    "user-read-currently-playing",
+    "user-read-playback-state",
+];
